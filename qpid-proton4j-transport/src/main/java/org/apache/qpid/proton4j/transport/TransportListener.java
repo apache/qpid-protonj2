424304b299f619bd746d00b0c161a572deb213f3
Timothy Bish <tabish121@gmail.com>
2017-10-27 17:56:45 -0400

    Update the package name to reflect what is there

--- qpid-proton4j-transport/src/main/java/org/apache/qpid/proton4j/transport/TransportListener.java
+++ qpid-proton4j-transport/src/main/java/org/apache/qpid/proton4j/transport/TransportListener.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.qpid.proton4j.amqp;
+package org.apache.qpid.proton4j.transport;
 
 import org.apache.qpid.proton4j.amqp.transport.Performative;
 
