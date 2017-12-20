2f9b6bbee9e8c6e538e758343e07e297508983e4
Timothy Bish <tabish121@gmail.com>
2017-12-20 16:54:22 -0500

    Add a deeper array test

--- qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/ArrayTypeCodecTest.java
+++ qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/ArrayTypeCodecTest.java
@@ -92,6 +92,49 @@ public class ArrayTypeCodecTest extends CodecTestSupport {
     }
 
     @Test
+    public void testArrayOfArraysOfArraysOfShortTypes() throws IOException {
+        ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
+
+        final int size = 10;
+
+        Object[][][] source = new Object[2][2][size];
+        for (int i = 0; i < source.length; ++i) {
+            for (int j = 0; j < source[i].length; ++j) {
+                for (int k = 0; k < source[i][j].length; ++k) {
+                    source[i][j][k] = (short) k;
+                }
+             }
+        }
+
+        encoder.writeArray(buffer, encoderState, source);
+
+        Object result = decoder.readObject(buffer, decoderState);
+        assertNotNull(result);
+        assertTrue(result.getClass().isArray());
+
+        Object[] resultArray = (Object[]) result;
+
+        assertNotNull(resultArray);
+        assertEquals(2, resultArray.length);
+
+        for (int i = 0; i < resultArray.length; ++i) {
+            assertTrue(resultArray[i].getClass().isArray());
+
+            Object[] dimension2 = (Object[]) resultArray[i];
+            assertEquals(2, dimension2.length);
+
+            for (int j = 0; j < dimension2.length; ++j) {
+                short[] dimension3 = (short[]) dimension2[j];
+                assertEquals(size, dimension3.length);
+
+                for (int k = 0; k < dimension3.length; ++k) {
+                    assertEquals(source[i][j][k], dimension3[k]);
+                }
+             }
+        }
+    }
+
+    @Test
     public void testWriteArrayOfArraysStrings() throws IOException {
         ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
 
