e5088b1b1708281cced5c3e7e72347dd260e63e1
Timothy Bish <tabish121@gmail.com>
2018-12-04 18:49:44 -0500

    Adds additional string encode and decode tests
    Tests that the encoder and decoder produce valid results for multi-byte
    code points

--- qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/primitives/StringTypeCodecTest.java
+++ qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/primitives/StringTypeCodecTest.java
@@ -22,6 +22,12 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
+import java.lang.Character.UnicodeBlock;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.qpid.proton4j.buffer.ProtonBuffer;
 import org.apache.qpid.proton4j.buffer.ProtonByteBufferAllocator;
@@ -30,6 +36,8 @@ import org.apache.qpid.proton4j.codec.EncodingCodes;
 import org.junit.Test;
 
 public class StringTypeCodecTest extends CodecTestSupport {
+
+    private static final List<String> TEST_DATA = generateTestData();
 
     private final String SMALL_STRING_VALUIE = "Small String";
     private final String LARGE_STRING_VALUIE = "Large String: " +
@@ -116,7 +124,6 @@ public class StringTypeCodecTest extends CodecTestSupport {
     }
 
     private void doTestDecodeStringOfZeroLengthWithGivenEncoding(byte encodingCode) throws IOException {
-
         ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
 
         // Manually encode the type we want.
@@ -126,5 +133,84 @@ public class StringTypeCodecTest extends CodecTestSupport {
         String result = decoder.readString(buffer, decoderState);
         assertNotNull(result);
         assertEquals("", result);
+    }
+
+    @Test
+    public void testEncodeAndDecodeComplexStrings() throws IOException {
+        ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
+
+        for (final String input : TEST_DATA) {
+            encoder.writeString(buffer, encoderState, input);
+            String result = decoder.readString(buffer, decoderState);
+
+            buffer.clear();
+
+            assertEquals(input, result);
+        }
+    }
+
+    //----- Test support for string encodings --------------------------------//
+
+    private static List<String> generateTestData() {
+        return new LinkedList<String>() {
+            private static final long serialVersionUID = 7331717267070233454L;
+
+            {
+                // non-surrogate pair blocks
+                addAll(getAllStringsFromUnicodeBlocks(UnicodeBlock.BASIC_LATIN,
+                                                      UnicodeBlock.LATIN_1_SUPPLEMENT,
+                                                      UnicodeBlock.GREEK,
+                                                      UnicodeBlock.LETTERLIKE_SYMBOLS));
+                // blocks with surrogate pairs
+                addAll(getAllStringsFromUnicodeBlocks(UnicodeBlock.LINEAR_B_SYLLABARY,
+                                                      UnicodeBlock.MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS,
+                                                      UnicodeBlock.MUSICAL_SYMBOLS,
+                                                      UnicodeBlock.EMOTICONS,
+                                                      UnicodeBlock.PLAYING_CARDS,
+                                                      UnicodeBlock.BOX_DRAWING,
+                                                      UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS,
+                                                      UnicodeBlock.PRIVATE_USE_AREA,
+                                                      UnicodeBlock.SUPPLEMENTARY_PRIVATE_USE_AREA_A,
+                                                      UnicodeBlock.SUPPLEMENTARY_PRIVATE_USE_AREA_B));
+
+                // some additional combinations of characters that could cause problems to the encoder
+                String[] boxDrawing = getAllStringsFromUnicodeBlocks(UnicodeBlock.BOX_DRAWING).toArray(new String[0]);
+                String[] halfFullWidthForms = getAllStringsFromUnicodeBlocks(UnicodeBlock.HALFWIDTH_AND_FULLWIDTH_FORMS).toArray(new String[0]);
+                for (int i = 0; i < halfFullWidthForms.length; i++) {
+                    add(halfFullWidthForms[i] + boxDrawing[i % boxDrawing.length]);
+                }
+            }
+        };
+    }
+
+    /**
+     * Loop over all the chars in given {@link UnicodeBlock}s and return a {@link Set <String>}
+     * containing all the possible values as their {@link String} values.
+     *
+     * @param blocks
+     *        the {@link UnicodeBlock}s to loop over
+     *
+     * @return a {@link Set <String>} containing all the possible values as {@link String} values
+     */
+    private static Set<String> getAllStringsFromUnicodeBlocks(final UnicodeBlock... blocks) {
+        final Set<UnicodeBlock> blockSet = new HashSet<>(Arrays.asList(blocks));
+        final Set<String> strings = new HashSet<>();
+
+        for (int codePoint = 0; codePoint <= Character.MAX_CODE_POINT; codePoint++) {
+            if (blockSet.contains(UnicodeBlock.of(codePoint))) {
+                final int charCount = Character.charCount(codePoint);
+                final StringBuilder sb = new StringBuilder(charCount);
+                if (charCount == 1) {
+                    sb.append(String.valueOf((char) codePoint));
+                } else if (charCount == 2) {
+                    sb.append(Character.highSurrogate(codePoint));
+                    sb.append(Character.lowSurrogate(codePoint));
+                } else {
+                    throw new IllegalArgumentException("Character.charCount of " + charCount + " not supported.");
+                }
+                strings.add(sb.toString());
+            }
+        }
+        return strings;
     }
 }
