fef29c615e953d38db5166aaf3747a6aa7873187
Timothy Bish <tabish121@gmail.com>
2018-12-17 15:20:07 -0500

    Import more tests from old proton for AMQP types

--- qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/transport/ErrorConditionTypeCodecTest.java
+++ qpid-proton4j-codec/src/test/java/org/apache/qpid/proton4j/codec/transport/ErrorConditionTypeCodecTest.java
@@ -16,9 +16,13 @@
  */
 package org.apache.qpid.proton4j.codec.transport;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -33,29 +37,118 @@ public class ErrorConditionTypeCodecTest extends CodecTestSupport {
 
     @Test
     public void testEncodeDecodeType() throws Exception {
-       ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
+        ProtonBuffer buffer = ProtonByteBufferAllocator.DEFAULT.allocate();
 
-       ErrorCondition error = new ErrorCondition();
-       error.setCondition(Symbol.valueOf("amqp-error"));
-       error.setDescription("Something bad");
+        ErrorCondition error = new ErrorCondition();
+        error.setCondition(Symbol.valueOf("amqp-error"));
+        error.setDescription("Something bad");
 
-       Map<Object, Object> infoMap = new LinkedHashMap<>();
-       infoMap.put("1", true);
-       infoMap.put("2", "string");
+        Map<Object, Object> infoMap = new LinkedHashMap<>();
+        infoMap.put("1", true);
+        infoMap.put("2", "string");
 
-       error.setInfo(infoMap);
+        error.setInfo(infoMap);
 
-       encoder.writeObject(buffer, encoderState, error);
+        encoder.writeObject(buffer, encoderState, error);
 
-       final ErrorCondition result = (ErrorCondition) decoder.readObject(buffer, decoderState);
+        final ErrorCondition result = (ErrorCondition) decoder.readObject(buffer, decoderState);
 
-       assertNotNull(result);
-       assertNotNull(result.getCondition());
-       assertNotNull(result.getDescription());
-       assertNotNull(result.getInfo());
+        assertNotNull(result);
+        assertNotNull(result.getCondition());
+        assertNotNull(result.getDescription());
+        assertNotNull(result.getInfo());
 
-       assertEquals(Symbol.valueOf("amqp-error"), result.getCondition());
-       assertEquals("Something bad", result.getDescription());
-       assertEquals(infoMap, result.getInfo());
+        assertEquals(Symbol.valueOf("amqp-error"), result.getCondition());
+        assertEquals("Something bad", result.getDescription());
+        assertEquals(infoMap, result.getInfo());
+    }
+
+    @Test
+    public void testEqualityOfNewlyConstructed() {
+        ErrorCondition new1 = new ErrorCondition();
+        ErrorCondition new2 = new ErrorCondition();
+        assertErrorConditionsEqual(new1, new2);
+    }
+
+    @Test
+    public void testSameObject() {
+        ErrorCondition error = new ErrorCondition();
+        assertErrorConditionsEqual(error, error);
+    }
+
+    @Test
+    public void testConditionEquality() {
+        String symbolValue = "symbol";
+
+        ErrorCondition same1 = new ErrorCondition();
+        same1.setCondition(Symbol.getSymbol(new String(symbolValue)));
+
+        ErrorCondition same2 = new ErrorCondition();
+        same2.setCondition(Symbol.getSymbol(new String(symbolValue)));
+
+        assertErrorConditionsEqual(same1, same2);
+
+        ErrorCondition different = new ErrorCondition();
+        different.setCondition(Symbol.getSymbol("other"));
+
+        assertErrorConditionsNotEqual(same1, different);
+    }
+
+    @Test
+    public void testConditionAndDescriptionEquality() {
+        String symbolValue = "symbol";
+        String descriptionValue = "description";
+
+        ErrorCondition same1 = new ErrorCondition();
+        same1.setCondition(Symbol.getSymbol(new String(symbolValue)));
+        same1.setDescription(new String(descriptionValue));
+
+        ErrorCondition same2 = new ErrorCondition();
+        same2.setCondition(Symbol.getSymbol(new String(symbolValue)));
+        same2.setDescription(new String(descriptionValue));
+
+        assertErrorConditionsEqual(same1, same2);
+
+        ErrorCondition different = new ErrorCondition();
+        different.setCondition(Symbol.getSymbol(symbolValue));
+        different.setDescription("other");
+
+        assertErrorConditionsNotEqual(same1, different);
+    }
+
+    @Test
+    public void testConditionDescriptionInfoEquality() {
+        String symbolValue = "symbol";
+        String descriptionValue = "description";
+
+        ErrorCondition same1 = new ErrorCondition();
+        same1.setCondition(Symbol.getSymbol(new String(symbolValue)));
+        same1.setDescription(new String(descriptionValue));
+        same1.setInfo(Collections.singletonMap(Symbol.getSymbol("key"), "value"));
+
+        ErrorCondition same2 = new ErrorCondition();
+        same2.setCondition(Symbol.getSymbol(new String(symbolValue)));
+        same2.setDescription(new String(descriptionValue));
+        same2.setInfo(Collections.singletonMap(Symbol.getSymbol("key"), "value"));
+
+        assertErrorConditionsEqual(same1, same2);
+
+        ErrorCondition different = new ErrorCondition();
+        different.setCondition(Symbol.getSymbol(symbolValue));
+        different.setDescription(new String(descriptionValue));
+        different.setInfo(Collections.singletonMap(Symbol.getSymbol("other"), "value"));
+
+        assertErrorConditionsNotEqual(same1, different);
+    }
+
+    private void assertErrorConditionsNotEqual(ErrorCondition error1, ErrorCondition error2) {
+        assertThat(error1, is(not(error2)));
+        assertThat(error2, is(not(error1)));
+    }
+
+    private void assertErrorConditionsEqual(ErrorCondition error1, ErrorCondition error2) {
+        assertEquals(error1, error2);
+        assertEquals(error2, error1);
+        assertEquals(error1.hashCode(), error2.hashCode());
     }
 }
