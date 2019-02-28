d886060f2aae85ab5029d02287f7afc625a92933
Timothy Bish <tabish121@gmail.com>
2019-02-28 11:57:16 -0500

    Add some basic code to the test support module for engine tests

---
+++ qpid-proton4j-test-support/src/main/java/org/apache/qpid/proton4j/engine/test/EngineTestDriverFactory.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.qpid.proton4j.engine.test;
+
+import org.apache.qpid.proton.engine.Engine;
+
+/**
+ * Factory for creating Proton Engine test driver instances.
+ */
+public abstract class EngineTestDriverFactory {
+
+    /**
+     * Create an EngineTestDriver linked to the given Engine and configure it for use in tests.
+     *
+     * @param engine
+     *      The engine implementation to test.
+     *
+     * @return an engine test driver to use when testing the engine implementation.
+     */
+    public static EngineTestDriver createDriver(Engine engine) {
+        return new EngineTestDriver(engine);
+    }
+}
