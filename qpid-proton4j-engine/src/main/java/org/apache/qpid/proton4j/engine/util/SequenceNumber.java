4d8603c11fc0b597a9ff64124924808f9d51463a
Timothy Bish <tabish121@gmail.com>
2019-03-29 14:16:30 -0400

    Possible future utility class for handles etc

---
+++ qpid-proton4j-engine/src/main/java/org/apache/qpid/proton4j/engine/util/SequenceNumber.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.qpid.proton4j.engine.util;
+
+/**
+ * A mutable sequence that represents an unsigned integer type underneath
+ */
+public class SequenceNumber extends Number implements Comparable<SequenceNumber> {
+
+    private static final long serialVersionUID = -1337181254740481576L;
+
+    private int sequence;
+
+    /**
+     * Create a new sequence starting at the given value.
+     *
+     * @param startValue
+     *      The starting value of this unsigned integer sequence
+     */
+    public SequenceNumber(int startValue) {
+        this.sequence = startValue;
+    }
+
+    /**
+     * Add one to the sequence value.
+     *
+     * @return this sequence.
+     */
+    public SequenceNumber increment() {
+        sequence++;
+        return this;
+    }
+
+    /**
+     * Subtract one to the sequence value.
+     *
+     * @return this sequence.
+     */
+    public SequenceNumber decrement() {
+        sequence--;
+        return this;
+    }
+
+    @Override
+    public int intValue() {
+        return sequence;
+    }
+
+    @Override
+    public long longValue() {
+        return Integer.toUnsignedLong(sequence);
+    }
+
+    @Override
+    public float floatValue() {
+        return longValue();
+    }
+
+    @Override
+    public double doubleValue() {
+        return longValue();
+    }
+
+    @Override
+    public int compareTo(SequenceNumber other) {
+        return Integer.compareUnsigned(sequence, other.sequence);
+    }
+
+    public int compareTo(int other) {
+        return Integer.compareUnsigned(sequence, other);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof SequenceNumber) {
+            return ((SequenceNumber) other).sequence == this.sequence;
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(sequence);
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toUnsignedString(sequence);
+    }
+}
