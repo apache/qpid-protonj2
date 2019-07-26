98133ca248cbceff6c7fc985ca02c003f8c3ea9a
Timothy Bish <tabish121@gmail.com>
2019-07-26 18:09:23 -0400

    Add some basic structure for client delivery and delivery tracker

---
+++ protonj2-clients/amqperative-client/src/main/java/org/messaginghub/amqperative/client/ClientDelivery.java
@@ -0,0 +1,116 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.messaginghub.amqperative.client;
+
+import org.apache.qpid.proton4j.amqp.messaging.Accepted;
+import org.apache.qpid.proton4j.amqp.messaging.Modified;
+import org.apache.qpid.proton4j.amqp.messaging.Rejected;
+import org.apache.qpid.proton4j.amqp.messaging.Released;
+import org.apache.qpid.proton4j.amqp.transactions.TransactionalState;
+import org.apache.qpid.proton4j.engine.IncomingDelivery;
+import org.messaginghub.amqperative.Delivery;
+import org.messaginghub.amqperative.DeliveryState;
+import org.messaginghub.amqperative.Message;
+
+/**
+ * Client inbound delivery object.
+ */
+public class ClientDelivery implements Delivery {
+
+    private final IncomingDelivery protonDelivery;
+
+    /**
+     * Creates a new client delivery object linked to the given {@link IncomingDelivery}
+     * instance.
+     *
+     * @param delivery
+     *      The proton incoming delivery that backs this client delivery facade.
+     */
+    ClientDelivery(IncomingDelivery delivery) {
+        this.protonDelivery = delivery;
+    }
+
+    @Override
+    public Message getMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Delivery accept() {
+        protonDelivery.disposition(Accepted.getInstance());
+        return this;
+    }
+
+    @Override
+    public Delivery disposition(DeliveryState state, boolean settle) {
+        org.apache.qpid.proton4j.amqp.transport.DeliveryState protonState = null;
+        if (state != null) {
+            // TODO - Create simpler DeliveryState object for client side ?
+            switch (state.getType()) {
+                case ACCEPTED:
+                    protonState = Accepted.getInstance();
+                case MODIFIED:
+                    protonState = new Modified();
+                case REJECTED:
+                    protonState = new Rejected();
+                case RELEASED:
+                    protonState = Released.getInstance();
+                case TRANSACTIONAL:
+                    protonState = new TransactionalState();
+                default:
+                    throw new IllegalArgumentException("Unknown DeliveryState type given");
+            }
+        }
+
+        protonDelivery.disposition(protonState, settle);
+        return this;
+    }
+
+    @Override
+    public Delivery settle() {
+        protonDelivery.settle();
+        return this;
+    }
+
+    @Override
+    public DeliveryState getLocalState() {
+        // TODO - Create simple mapping builder in our DeliveryState implementation
+        return null;
+    }
+
+    @Override
+    public DeliveryState getRemoteState() {
+        // TODO - Create simple mapping builder in our DeliveryState implementation
+        return null;
+    }
+
+    @Override
+    public boolean isRemotelySettled() {
+        return protonDelivery.isRemotelySettled();
+    }
+
+    @Override
+    public byte[] getTag() {
+        return protonDelivery.getTag();
+    }
+
+    @Override
+    public int getMessageFormat() {
+        return protonDelivery.getMessageFormat();
+    }
+}
---
+++ protonj2-clients/amqperative-client/src/main/java/org/messaginghub/amqperative/client/ClientTracker.java
@@ -0,0 +1,69 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.messaginghub.amqperative.client;
+
+import org.apache.qpid.proton4j.engine.OutgoingDelivery;
+import org.messaginghub.amqperative.DeliveryState;
+import org.messaginghub.amqperative.Message;
+import org.messaginghub.amqperative.Tracker;
+
+/**
+ * Client outgoing delivery tracker object.
+ */
+public class ClientTracker implements Tracker {
+
+    private final OutgoingDelivery delivery;
+
+    /**
+     * Create an instance of a client outgoing delivery tracker.
+     *
+     * @param delivery
+     *      The proton outgoing delivery object that backs this tracker.
+     */
+    ClientTracker(OutgoingDelivery delivery) {
+        this.delivery = delivery;
+    }
+
+    @Override
+    public Message getMessage() {
+        // TODO Are we holding a message or just tracking the outgoing delivery object ?
+        return null;
+    }
+
+    @Override
+    public DeliveryState getRemoteState() {
+        // TODO - Create simple mapping builder in our DeliveryState implementation
+        return null;
+    }
+
+    @Override
+    public boolean isRemotelySettled() {
+        return delivery.isRemotelySettled();
+    }
+
+    @Override
+    public Tracker settle() {
+        delivery.settle();
+        return this;
+    }
+
+    @Override
+    public byte[] getTag() {
+        delivery.getTag();
+        return null;
+    }
+}
