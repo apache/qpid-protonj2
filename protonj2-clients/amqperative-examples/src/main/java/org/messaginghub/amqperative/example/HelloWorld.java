77ce17d1295cb128f53e55703c9c78c1dd860615
Timothy Bish <tabish121@gmail.com>
2019-07-30 09:37:31 -0400

    Add some basic example code for a hello world type sample

--- protonj2-clients/amqperative-examples/src/main/java/org/messaginghub/amqperative/example/HelloWorld.java
+++ protonj2-clients/amqperative-examples/src/main/java/org/messaginghub/amqperative/example/HelloWorld.java
@@ -20,12 +20,53 @@
  */
 package org.messaginghub.amqperative.example;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.messaginghub.amqperative.Connection;
+import org.messaginghub.amqperative.Container;
+import org.messaginghub.amqperative.ContainerOptions;
+import org.messaginghub.amqperative.Delivery;
+import org.messaginghub.amqperative.Message;
+import org.messaginghub.amqperative.Receiver;
+import org.messaginghub.amqperative.Sender;
+import org.messaginghub.amqperative.Tracker;
+
 public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
 
         try {
+            String brokerHost = "localhost";
+            int brokerPort = 5672;
+            String queueName = "test-queue";
 
+            ContainerOptions options = new ContainerOptions();
+            options.setContainerId(UUID.randomUUID().toString());
+            Container container = Container.create(options);
+
+            Connection connection = container.createConnection(brokerHost, brokerPort);
+            Sender sender = connection.createSender(queueName);
+            sender.openFuture().get(5, TimeUnit.SECONDS);
+
+            Message<String> message = Message.create("Hello World").setDurable(true);
+            Tracker tracker = sender.send(message);
+
+            tracker.settle();
+
+            Receiver receiver = connection.createReceiver(queueName);
+            receiver.openFuture().get(5, TimeUnit.SECONDS);
+            receiver.addCredit(1);
+
+            Delivery delivery = receiver.receive();
+            if (delivery != null) {
+                Message<?> received = delivery.getMessage();
+                if (received.getBody() instanceof String) {
+                    System.out.println(received.getBody());
+                } else {
+                    System.out.println("Unexpected body type: " + received.getBody());
+                }
+            }
         } catch (Exception exp) {
             System.out.println("Caught exception, exiting.");
             exp.printStackTrace(System.out);
