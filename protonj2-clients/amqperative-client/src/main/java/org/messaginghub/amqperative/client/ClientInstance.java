34597615771cef625ff7f7eee18a8ca81690d773
Timothy Bish <tabish121@gmail.com>
2019-08-19 18:18:55 -0400

    Add some more implemenation for client stop

--- protonj2-clients/amqperative-client/src/main/java/org/messaginghub/amqperative/client/ClientInstance.java
+++ protonj2-clients/amqperative-client/src/main/java/org/messaginghub/amqperative/client/ClientInstance.java
@@ -16,19 +16,25 @@
  */
 package org.messaginghub.amqperative.client;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.messaginghub.amqperative.Client;
 import org.messaginghub.amqperative.Connection;
 import org.messaginghub.amqperative.ConnectionOptions;
-import org.messaginghub.amqperative.Client;
 import org.messaginghub.amqperative.ContainerOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Container of {@link Connection} instances that are all created with the same
  * container parent and therefore share the same container Id.
  */
 public class ClientInstance implements Client {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ClientInstance.class);
 
     private final ClientContainerOptions options;
     private final Map<String, ClientConnection> connections = new HashMap<>();
@@ -58,6 +64,16 @@ public class ClientInstance implements Client {
 
     @Override
     public Client stop() {
+        synchronized (connections) {
+            List<Connection> connectionsView = new ArrayList<>(connections.values());
+            for (Connection connection : connectionsView) {
+                try {
+                    connection.close().get();
+                } catch (Throwable ignored) {
+                    LOG.trace("Error while closing connection, ignoring", ignored);
+                }
+            }
+        }
         return this;
     }
 
@@ -70,12 +86,14 @@ public class ClientInstance implements Client {
 
     void addConnection(ClientConnection connection) {
         synchronized (connections) {
+            checkStopped();
             this.connections.put(connection.getId(), connection);
         }
     }
 
     void removeConnection(ClientConnection connection) {
         synchronized (connections) {
+            checkStopped();
             this.connections.remove(connection.getId());
         }
     }
