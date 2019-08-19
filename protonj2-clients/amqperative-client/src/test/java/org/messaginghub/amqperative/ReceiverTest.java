39db3a5e61093f050821d1815655a0972edb1ab9
Timothy Bish <tabish121@gmail.com>
2019-08-19 17:57:09 -0400

    Add a couple basic sender and receiver tests

---
+++ protonj2-clients/amqperative-client/src/test/java/org/messaginghub/amqperative/ReceiverTest.java
@@ -0,0 +1,50 @@
+package org.messaginghub.amqperative;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.qpid.proton4j.amqp.driver.netty.NettyTestPeer;
+import org.apache.qpid.proton4j.amqp.transport.Role;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ReceiverTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReceiverTest.class);
+
+    @Test(timeout = 60000)
+    public void testCreateConnectionString() throws Exception {
+        try (NettyTestPeer peer = new NettyTestPeer()) {
+            peer.expectAMQPHeader().respondWithAMQPHeader();
+            peer.expectOpen().respond();
+            peer.expectBegin().respond();
+            peer.expectAttach().withRole(Role.RECEIVER).respond();  // TODO match other options
+            peer.expectDetach().withClosed(true).respond();
+            peer.expectClose().respond();
+            peer.start();
+
+            URI remoteURI = peer.getServerURI();
+
+            LOG.info("Connect test started, peer listening on: {}", remoteURI);
+
+            Client container = Client.create();
+            Connection connection = container.createConnection(remoteURI.getHost(), remoteURI.getPort());
+
+            connection.openFuture().get(10, TimeUnit.SECONDS);
+
+            Session session = connection.createSession();
+            session.openFuture().get(10, TimeUnit.SECONDS);
+
+            Receiver receiver = session.createReceiver("test-queue");
+            receiver.openFuture().get(10, TimeUnit.SECONDS);
+            receiver.close().get(10, TimeUnit.SECONDS);
+
+            connection.close().get(10, TimeUnit.SECONDS);
+
+            peer.waitForScriptToComplete(5, TimeUnit.SECONDS);
+
+            LOG.info("Receiver test completed normally");
+        }
+    }
+}
---
+++ protonj2-clients/amqperative-client/src/test/java/org/messaginghub/amqperative/SenderTest.java
@@ -0,0 +1,50 @@
+package org.messaginghub.amqperative;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.qpid.proton4j.amqp.driver.netty.NettyTestPeer;
+import org.apache.qpid.proton4j.amqp.transport.Role;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SenderTest {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SenderTest.class);
+
+    @Test(timeout = 60000)
+    public void testCreateConnectionString() throws Exception {
+        try (NettyTestPeer peer = new NettyTestPeer()) {
+            peer.expectAMQPHeader().respondWithAMQPHeader();
+            peer.expectOpen().respond();
+            peer.expectBegin().respond();
+            peer.expectAttach().withRole(Role.SENDER).respond();  // TODO match other options
+            peer.expectDetach().withClosed(true).respond();
+            peer.expectClose().respond();
+            peer.start();
+
+            URI remoteURI = peer.getServerURI();
+
+            LOG.info("Sender test started, peer listening on: {}", remoteURI);
+
+            Client container = Client.create();
+            Connection connection = container.createConnection(remoteURI.getHost(), remoteURI.getPort());
+
+            connection.openFuture().get(10, TimeUnit.SECONDS);
+
+            Session session = connection.createSession();
+            session.openFuture().get(10, TimeUnit.SECONDS);
+
+            Sender sender = session.createSender("test-queue");
+            sender.openFuture().get(10, TimeUnit.SECONDS);
+            sender.close().get(10, TimeUnit.SECONDS);
+
+            connection.close().get(10, TimeUnit.SECONDS);
+
+            peer.waitForScriptToComplete(5, TimeUnit.SECONDS);
+
+            LOG.info("Sender test completed normally");
+        }
+    }
+}
