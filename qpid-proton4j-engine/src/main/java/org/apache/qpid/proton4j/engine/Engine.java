3712a3177ffd7448c02faa50e0a45d447c90d63e
Timothy Bish <tabish121@gmail.com>
2019-09-11 11:51:07 -0400

    Clean up some docs and spelling errors add a few extra notes to docs

--- qpid-proton4j-engine/src/main/java/org/apache/qpid/proton4j/engine/Engine.java
+++ qpid-proton4j-engine/src/main/java/org/apache/qpid/proton4j/engine/Engine.java
@@ -31,21 +31,17 @@ import org.apache.qpid.proton4j.engine.exceptions.ProtonException;
  */
 public interface Engine extends Consumer<ProtonBuffer> {
 
-    //----- Engine state
-
     /**
      * Returns true if the engine is accepting more input.
      * <p>
      * When false any attempts to write more data into the engine will result in an
      * error being returned from the write operation.
      *
-     * TODO What is the best why to reflect the error, how do we trigger changes
+     * TODO What is the best why to reflect the state, how do we trigger changes
      *
      * @return true if the engine is current accepting more input.
      */
     boolean isWritable();
-
-    // TODO decide on terminology for state, shutdown, closed, etc
 
     /**
      * @return true if the Engine has been shutdown and is no longer usable.
@@ -58,17 +54,16 @@ public interface Engine extends Consumer<ProtonBuffer> {
     EngineState state();
 
     /**
+     * Gets the {@link Connection} instance that is associated with this {@link Engine} instance.
+     * It is valid for an engine implementation to not return a {@link Connection} instance prior
+     * to the engine having been started.
+     *
      * @return the {@link Connection} that is linked to this engine instance.
      */
     Connection getConnection();
 
-    //----- Engine control APIs
-
     /**
-     * Starts the engine
-     * <p>
-     * Once started the engine will signal the provided handler that the engine is started and
-     * provide a new Connection instance.
+     * Starts the engine and returns the {@link Connection} instance that is bound to this Engine.
      *
      * @return the Connection instance that is linked to this {@link Engine}
      */
@@ -78,17 +73,12 @@ public interface Engine extends Consumer<ProtonBuffer> {
      * Orderly shutdown of the engine, any open connection and associated sessions and
      * session linked end points will be closed.
      *
-     * TODO - Errors
-     *
      * @return this Engine
      */
     Engine shutdown();
 
     /**
      * Provide data input for this Engine from some external source.
-     * <p>
-     * The provided {@link ProtonBuffer} should be a non-pooled buffer which the Engine
-     * will take ownership of and should not be modified or reused by the caller.
      *
      * @param input
      *      The data to feed into to Engine.
@@ -103,9 +93,6 @@ public interface Engine extends Consumer<ProtonBuffer> {
 
     /**
      * Provide data input for this Engine from some external source.
-     * <p>
-     * The provided {@link ProtonBuffer} should be a non-pooled buffer which the Engine
-     * will take ownership of and should not be modified or reused by the caller.
      *
      * @param input
      *      The data to feed into to Engine.
@@ -165,14 +152,36 @@ public interface Engine extends Consumer<ProtonBuffer> {
      */
     void tickAuto(ScheduledExecutorService executor);
 
-    //----- Engine configuration and state
+    /**
+     * Gets the EnginePipeline for this Engine.
+     *
+     * @return the {@link EnginePipeline} for this {@link Engine}.
+     */
+    EnginePipeline pipeline();
+
+    /**
+     * Gets the Configuration for this engine.
+     *
+     * @return the configuration object for this engine.
+     */
+    EngineConfiguration configuration();
+
+    /**
+     * Gets the SASL context for this engine, if no SASL layer is configured then a
+     * default no-op context must be returned that indicates this.
+     *
+     * @return the SASL context for the engine.
+     */
+    EngineSaslContext saslContext();
+
+    //----- Engine event points
 
     /**
      * Sets a handler instance that will be notified when data from the engine is ready to
      * be written to some output sink (socket etc).
      *
      * @param output
-     *      The {@link ProtonBuffer} handler instance that perform IO for the engine output.
+     *      The {@link ProtonBuffer} handler instance that performs IO for the engine output.
      *
      * @return this Engine
      */
@@ -183,7 +192,7 @@ public interface Engine extends Consumer<ProtonBuffer> {
      * be written to some output sink (socket etc).
      *
      * @param consumer
-     *      The {@link ProtonBuffer} consumer instance that perform IO for the engine output.
+     *      The {@link ProtonBuffer} consumer instance that performs IO for the engine output.
      *
      * @return this Engine
      */
@@ -204,27 +213,5 @@ public interface Engine extends Consumer<ProtonBuffer> {
      * @return this Engine
      */
     Engine errorHandler(EventHandler<ProtonException> engineFailure);
-
-    /**
-     * Gets the EnginePipeline for this Engine.
-     *
-     * @return the {@link EnginePipeline} for this {@link Engine}.
-     */
-    EnginePipeline pipeline();
-
-    /**
-     * Gets the Configuration for this engine.
-     *
-     * @return the configuration object for this engine.
-     */
-    EngineConfiguration configuration();
-
-    /**
-     * Gets the SASL context for this engine, if not SASL layer is configured then a
-     * default no-op context should be returned that indicates this.
-     *
-     * @return the SASL context for the engine.
-     */
-    EngineSaslContext saslContext();
 
 }
