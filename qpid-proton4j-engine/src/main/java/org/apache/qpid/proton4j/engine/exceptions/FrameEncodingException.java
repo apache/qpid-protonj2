725bd4f1474b33b4a5ef854576f2505a9e0da713
Timothy Bish <tabish121@gmail.com>
2020-05-27 18:05:05 -0400

    Add new class of exception to deal with frame encode and decoder errors

---
+++ qpid-proton4j-engine/src/main/java/org/apache/qpid/proton4j/engine/exceptions/FrameEncodingException.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.qpid.proton4j.engine.exceptions;
+
+/**
+ * Exception thrown when the engine cannot encode a frame from a given performative
+ * and or payload combination.
+ */
+public class FrameEncodingException extends ProtocolViolationException {
+
+    private static final long serialVersionUID = -5392939106677054003L;
+
+    public FrameEncodingException() {
+    }
+
+    public FrameEncodingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FrameEncodingException(String message) {
+        super(message);
+    }
+
+    public FrameEncodingException(Throwable cause) {
+        super(cause);
+    }
+}
