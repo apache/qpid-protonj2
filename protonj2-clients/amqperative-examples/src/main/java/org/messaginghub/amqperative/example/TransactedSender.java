c95113418b2e454f8b216f864e24fcaea352ec6f
Timothy Bish <tabish121@gmail.com>
2020-06-10 18:50:35 -0400

    Add basic transacted sender example

---
+++ protonj2-clients/amqperative-examples/src/main/java/org/messaginghub/amqperative/example/TransactedSender.java
@@ -0,0 +1,63 @@
+/*
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ *
+ */
+package org.messaginghub.amqperative.example;
+
+import java.util.UUID;
+
+import org.messaginghub.amqperative.Client;
+import org.messaginghub.amqperative.ClientOptions;
+import org.messaginghub.amqperative.Connection;
+import org.messaginghub.amqperative.Message;
+import org.messaginghub.amqperative.Sender;
+import org.messaginghub.amqperative.Session;
+
+public class TransactedSender {
+
+    public static void main(String[] args) throws Exception {
+
+        try {
+            String brokerHost = "localhost";
+            int brokerPort = 5672;
+            String address = "examples";
+
+            ClientOptions options = new ClientOptions();
+            options.id(UUID.randomUUID().toString());
+            Client client = Client.create(options);
+
+            Connection connection = client.connect(brokerHost, brokerPort);
+            Session session = connection.openSession();
+
+            session.begin();
+
+            Sender sender = session.openSender(address);
+            sender.send(Message.create("Hello World"));
+
+            session.commit();
+
+            connection.close().get();
+        } catch (Exception exp) {
+            System.out.println("Caught exception, exiting.");
+            exp.printStackTrace(System.out);
+            System.exit(1);
+        } finally {
+        }
+    }
+}
