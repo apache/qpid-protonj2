bc37e02965ba2268923756edccc135f67a310657
Timothy Bish <tabish121@gmail.com>
2021-03-08 15:21:24 -0500

    Disable io_uring test for now as it can be unstable in CI currently

--- protonj2-client/src/test/java/org/apache/qpid/protonj2/client/transport/TcpTransportTest.java
+++ protonj2-client/src/test/java/org/apache/qpid/protonj2/client/transport/TcpTransportTest.java
@@ -787,6 +787,7 @@ public class TcpTransportTest extends ImperativeClientTestCase {
         assertTrue(data.isEmpty());
     }
 
+    @Disabled("Disabled until the io_uring support matures, can cause CI issues")
     @Test
     public void testConnectToServerWithIOUringEnabled() throws Exception {
         doTestIORingSupport(true);
