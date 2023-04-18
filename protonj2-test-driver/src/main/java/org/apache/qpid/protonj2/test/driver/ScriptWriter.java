f1525208ee5ee4b571adebc1a60ca440ceaccc14
Timothy Bish <tabish121@gmail.com>
2023-04-18 16:15:56 -0400

    PROTON-2711 Add missing header expectation to test script utility API
    Fixes intermittent failure in CI when the header arrived before the
    connection closed in new tests.

--- protonj2-test-driver/src/main/java/org/apache/qpid/protonj2/test/driver/ScriptWriter.java
+++ protonj2-test-driver/src/main/java/org/apache/qpid/protonj2/test/driver/ScriptWriter.java
@@ -563,6 +563,7 @@ public abstract class ScriptWriter {
         remoteSaslInit().withMechanism("ANONYMOUS").queue();
         expectSaslOutcome().withCode(SaslCode.OK);
         remoteAMQPHeader().queue();
+        expectAMQPHeader();
     }
 
     /**
